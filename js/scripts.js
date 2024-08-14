88fa7abddd8eb37a597559c489942bbf52f2a411
Amidu99 <amidushamika@gmail.com>
2024-08-14 22:33:19 +0530

    finalize scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,16 @@ AOS.init();
 document.addEventListener("DOMContentLoaded", ()=>{
     console.log("Start redirecting...");
 });
+
 window.onload = () =>{
     console.log("Loading...");
     setInterval(close_loader,3000);
 }
+
 function close_loader() {
     $("#preloader").css({"display": "none"});
 }
+
 $(window).ready(() => {
     console.log("Done.");
 });
